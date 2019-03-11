0c847d508d863595ccbb29306775ce7a826df28f
keenanwoodall <keenanwoodall@gmail.com>
2019-03-11 03:46:51 -0500

    Created simple effect and test scene

---
+++ Code/GrappleEffect.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class GrappleEffect : MonoBehaviour
+{
+	[Header ("Animation")]
+	public AnimationCurve effectOverTime = new AnimationCurve ();
+	[Header ("Curve")]
+	public AnimationCurve curve = new AnimationCurve ();
+	public float curveAmount = 1f;
+	public float curveSize = 3f;
+	public AnimationCurve curveEffectCurve = new AnimationCurve ();
+	public float scrollSpeed = 5f;
+	public int segments = 100;
+
+	private RaycastHit hit;
+	private float scrollOffset = 0f;
+	private LineRenderer lineRenderer;
+
+	private void Awake ()
+	{
+		lineRenderer = GetComponent<LineRenderer> ();
+	}
+
+	public void Do ()
+	{
+		lineRenderer.enabled = Physics.Raycast (transform.position, transform.forward, out hit);
+
+		scrollOffset = 0f;
+		if (lineRenderer.positionCount != segments)
+			lineRenderer.positionCount = segments;
+	}
+
+	private void Update ()
+	{
+		if (!lineRenderer.enabled)
+			return;
+
+		scrollOffset += scrollSpeed * Time.deltaTime;
+
+		var difference = hit.point - transform.position;
+		var direction = difference.normalized;
+		var distance = difference.magnitude;
+
+		for (int i = 0; i < lineRenderer.positionCount; i++)
+		{
+			var t = (float)i / lineRenderer.positionCount;
+			var position = transform.position;
+			var forwardOffset = direction * (t * distance);
+			position += forwardOffset;
+
+			var verticalOffset = transform.up * curve.Evaluate (forwardOffset.magnitude / curveSize) * curveAmount;
+			verticalOffset *= effectOverTime.Evaluate (scrollOffset);
+			verticalOffset *= curveEffectCurve.Evaluate (t);
+			position += verticalOffset;
+
+			lineRenderer.SetPosition (i, position);
+		}
+	}
+}
---
+++ Examples/GrappleTester.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class GrappleTester : MonoBehaviour
+{
+	[SerializeField] private GrappleEffect grappleEffect;
+
+	private new Camera camera;
+
+	private void Awake ()
+	{
+		camera = Camera.main;
+
+		if (grappleEffect == null)
+			grappleEffect = GetComponent<GrappleEffect> ();
+	}
+
+	private void Update ()
+	{
+		if (Input.GetMouseButtonDown (0))
+		{
+			RaycastHit hit;
+			if (Physics.Raycast (camera.ScreenPointToRay (Input.mousePosition), out hit))
+			{
+				grappleEffect.transform.LookAt (hit.point);
+				grappleEffect.Do ();
+			}
+		}
+	}
+}
